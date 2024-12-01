bfa45c21481c1605791404b49c4c17db8162a1a8
em1ru <gabemile7@gmail.com>
2024-12-01 20:48:49 -0300

    Add Procfile for Heroku deployment

--- DigitalShop/src/environments/environment.ts
+++ DigitalShop/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api_host: 'http://localhost:8000/',
-  productsListAPI: 'http://localhost:8000/products/list/',
-  loginAPI: 'http://localhost:8000/account/token-auth/',
+  api_host: 'https://inf1407-backend-1d718e303f52.herokuapp.com/',
+  productsListAPI: 'https://inf1407-backend-1d718e303f52.herokuapp.com/products/list/',
+  loginAPI: 'https://inf1407-backend-1d718e303f52.herokuapp.com/account/token-auth/',
 };
